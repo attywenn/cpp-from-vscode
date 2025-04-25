1ffda3fd9a0aaf71ef50fa405c156893cbfe0c0f
Wency Herrera <wnciplays@gmail.com>
2025-04-25 15:05:23 +0700

    Hey

--- system.cpp
+++ system.cpp
@@ -18,17 +18,17 @@ int mMenu () {
     char ourDate;
 
     while (1) {
-        cout << "When was the day that we started our relationship? "<<endl;
+        cout << "\nWhen was the day that we started our relationship? "<<endl;
         cout << "A. December 9, 1854\nB. August 5, 89 BC\nC. March 5, 2025 "<<endl;
         cout << "Your answer: ";
         cin >> ourDate;
 
         if (ourDate == 'a' || ourDate == 'A') {
-            cout<<"Sana kinain ko na lang ang El Fili."<<endl;
+            cout<<"\nSana kinain ko na lang ang El Fili."<<endl;
         } else if (ourDate == 'b' || ourDate == 'B') {
-            cout << "Sarap magpapako sa krus lab"<<endl;
+            cout << "\nSarap magpapako sa krus lab"<<endl;
         } else if (ourDate == 'c' || ourDate == 'C') {
-            cout << "Dahil tama ka, automatically redirect kita sa gift ko sa 'yo"<<endl;
+            cout << "\nDahil tama ka, automatically redirect kita sa gift ko sa 'yo"<<endl;
             system("start www.facebook.com/herrerawency");
             break;
         } else {
